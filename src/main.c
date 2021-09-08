7ef927639a2054c488246a7dff9566e72508fc3b
jazielloureiro <jazielloureiro17@gmail.com>
2021-09-08 10:19:22 -0300

    Delete old code

--- src/main.c
+++ src/main.c
@@ -1,27 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "validate.h"
-
-void help(){
-	puts("Syntax:\n\t./bc [OPTION] [NUMBER]");
-}
-
 int main(int argc, char **argv){
-	if(argc > 3)
-		puts("You've entered too many arguments.");
-	else if(argc == 1 || strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
-		help();
-	else if(strncmp(argv[1], "bin", 3) == 0){
-		validate_bin(argv[2]);
-	}else if(strncmp(argv[1], "oct", 3) == 0){
-		validate_oct(argv[2]);
-	}else if(strncmp(argv[1], "dec", 3) == 0){
-		validate_dec(argv[2]);
-	}else if(strncmp(argv[1], "hex", 3) == 0){
-		validate_hex(argv[2]);
-	}else
-		puts("You've entered an invalid option.");
-
 	return 0;
 }
--- src/validate.h
+++
@@ -1,9 +0,0 @@
-#ifndef VALIDATE_H
-#define VALIDATE_H
-
-void validate_bin(char *bin);
-void validate_oct(char *oct);
-void validate_dec(char *dec);
-void validate_hex(char *hex);
-
-#endif
