c94aa410f9f0df5b1008d01e247f25af03559422
jazielloureiro <jazielloureiro17@gmail.com>
2021-09-08 11:37:36 -0300

    Add to_decimal function

--- src/converter.c
+++ src/converter.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <string.h>
 
 #define UPPERCASE 55
 #define LOWERCASE 87
@@ -14,4 +15,13 @@ uint8_t get_int(char number) {
 
 uint64_t power(uint8_t base, uint8_t exp) {
 	return exp == 0? 1 : base * power(base, exp - 1);
+}
+
+uint64_t to_decimal(char *input, int radix) {
+	uint64_t decimal = 0;
+
+	for(int16_t i = strlen(input) - 1, e = 0; i >= 0; i--, e++)
+		decimal += get_int(input[i]) * power(radix, e);
+	
+	return decimal;
 }
