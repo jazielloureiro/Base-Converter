7a1df53f8d3a87cd45931400315408fa1c0b271b
jazielloureiro <jazielloureiro17@gmail.com>
2021-09-10 13:55:45 -0300

    Add input validation

--- src/converter.c
+++ src/converter.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
 
@@ -47,7 +48,22 @@ void to_base(uint64_t decimal, char *out, int base) {
 		out[i] = temp[temp_size];
 }
 
+bool is_input_valid(char *input, uint8_t base) {
+	for( ; *input != '\0'; input++)
+		if(get_int(*input) >= base)
+			return false;
+
+	return true;
+}
+
 void base_convert(char *in, uint8_t in_base, char *out, uint8_t out_base) {
-	uint64_t decimal = to_decimal(in, in_base);
-	to_base(decimal, out, out_base);
+	if(in_base >= 2 && in_base <= 36
+	   && out_base >= 2 && out_base <= 36
+	   && is_input_valid(in, in_base)
+	) {
+		uint64_t decimal = to_decimal(in, in_base);
+		to_base(decimal, out, out_base);
+	} else {
+		strcpy(out, "");
+	}
 }
